d35655afb1d79f1e555acef14468d37af156a7a7
John <johnnkonge2020@gmail.com>
2024-03-17 17:04:23 +0300

    re-applied migrations and created superuser

--- B.E/api/models.py
+++ B.E/api/models.py
@@ -1,3 +1,34 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
 # Create your models here.
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length=50)
+    email = models.EmailField(unique =True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.username
+    
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete = models.CASCADE)
+    full_name = models.CharField(max_length = 50)
+    phone_number = models.IntegerField(null = True)
+    verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.full_name
+    
+def create_user(sender,instance,created, **kwargs):
+    if created:
+        Profile.objects.create(user = instance)
+def save_user(sender, instance ,**kwargs):
+    instance.profile.save()
+
+post_save.connect(create_user, sender=User)
+post_save.connect(save_user, sender=User)
--- B.E/backend/settings.py
+++ B.E/backend/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     #3rdparty
     'rest_framework',
     'corsheaders',
-    'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
+    'jazzmin',
 ]
 
 REST_FRAMEWORK = {
@@ -191,3 +192,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'api.User'
